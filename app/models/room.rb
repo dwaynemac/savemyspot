33d321934430b53a662211ecd348ba2b9b75c5d4
Dwayne Macgowan <dwaynemac@gmail.com>
2016-06-27 23:27:33 -0300

    add rooms to dashboard

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,5 @@
 class Room < ApplicationRecord
   validates_presence_of :name
+
+  has_many :activities
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,5 @@
 class Teacher < ApplicationRecord
   validates_presence_of :name
+
+  has_many :activities
 end
