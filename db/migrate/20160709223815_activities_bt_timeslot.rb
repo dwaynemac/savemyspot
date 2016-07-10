4714cf1992910be4628229fc5d423bdea6d3e587
Dwayne Macgowan <dwaynemac@gmail.com>
2016-07-09 23:28:06 -0300

    fix migration

--- db/migrate/20160709223815_activities_bt_timeslot.rb
+++ db/migrate/20160709223815_activities_bt_timeslot.rb
@@ -1,5 +1,5 @@
 class ActivitiesBtTimeslot < ActiveRecord::Migration[5.0]
   def change
-    add_column :activities, :timeslot_id, :reference
+    add_column :activities, :timeslot, :reference
   end
 end
