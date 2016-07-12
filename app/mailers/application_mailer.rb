6cb6a2fc55ac810fa6ef9422cfb6c6ea81f49dae
Dwayne Macgowan <dwaynemac@gmail.com>
2016-07-11 23:18:58 -0300

    add from name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@derosefestival.com.ar'
+  default from: '"DeROSE Festival Buenos Aires" <no-reply@derosefestival.com.ar>'
   layout 'mailer'
 end
