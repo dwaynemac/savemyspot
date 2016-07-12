823b01825e53939ad33e98cf6d1f19b4a814c9d2
Dwayne Macgowan <dwaynemac@gmail.com>
2016-07-12 11:16:07 -0300

    remove unused validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,19 +10,12 @@ class Booking < ApplicationRecord
   validate :vacancies_available
 
   after_save :remove_other_bookings_on_timeslot
-  #validate :one_spot_per_timslot
 
   private
 
   def remove_other_bookings_on_timeslot
     if activity 
       Booking.joins(:activity).where(activities: { timeslot_id: activity.timeslot_id}, user_id: user_id).where("bookings.id <> ?",id).destroy_all
-    end
-  end
-
-  def one_spot_per_timslot
-    if activity && Booking.joins(:activity).where(activities: { timeslot_id: activity.timeslot_id}, user_id: user_id).exists?
-      self.errors.add(:base, :one_spot_per_timeslot)
     end
   end
 
