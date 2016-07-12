c5f80033b94ddbe0f2990e3f0f432fce3811560a
Dwayne Macgowan <dwaynemac@gmail.com>
2016-07-11 22:57:51 -0300

    deliver link to email

--- app/controllers/token_session_controller.rb
+++ app/controllers/token_session_controller.rb
@@ -1,7 +1,13 @@
 class TokenSessionController < Devise::SessionsController
 
   def create
-    render text: "you submitted email: #{params[:user][:email]}"
+    @user = User.find_by_email(params[:user][:email])
+    if @user
+      TokenMailer.login_link(@user).deliver_now
+    else
+      flash.alert = t('.email_not_registered')
+      redirect_to new_user_session_path
+    end
   end
 
 end
---
+++ app/mailers/token_mailer.rb
@@ -0,0 +1,14 @@
+class TokenMailer < ApplicationMailer
+  
+  def login_link(user)
+    @url = token_login_url(user)
+    mail(to: user.email, subject: t('.your_login_link'))
+  end
+
+  private
+
+  def token_login_url(user)
+    protocol = Rails.env.development?? 'http' : 'https'
+    root_url(protocol: protocol, user_email: user.email, user_token: user.authentication_token)
+  end
+end
