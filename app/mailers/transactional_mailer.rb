b975d74e47fdfabee1b66ee8be1de7b7cd522ca2
Dwayne Macgowan <dwaynemac@gmail.com>
2016-07-29 15:36:31 -0300

    confirmation mail content

--- app/mailers/transactional_mailer.rb
+++ app/mailers/transactional_mailer.rb
@@ -1,6 +1,7 @@
+# encoding: utf-8
 class TransactionalMailer < ApplicationMailer
   def confirmation(user)
     user.update_attribute(:sent_confirmation_email, true)
-    mail(to: user.email, subject: '')
+    mail(to: user.email, subject: '¡Tenés tu lugar en la Avant Première del DeROSE Festival 2016 Buenos Aires!')
   end
 end
